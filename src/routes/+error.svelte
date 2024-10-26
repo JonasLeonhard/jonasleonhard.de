1d681f7dc49a3ed22979f0699091df0aa7e0a337
jonas <jonasleonhardfl@gmail.com>
2024-10-26 11:05:27 +0200

    fix(error): add simple error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { page } from '$app/stores';
+	import { BatteryWarning } from 'lucide-svelte';
+</script>
+
+<div
+	class="container fixed left-[50%] top-[50%] mx-auto flex w-full -translate-x-[50%] flex-row justify-center gap-4"
+>
+	<BatteryWarning />
+	<h1><span class="text-accent">{$page.status}</span> {$page.error?.message}</h1>
+</div>
