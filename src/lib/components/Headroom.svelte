c6061cd39ac58e1194e016673af576b48e2c9f87
jonas <jonasleonhardfl@gmail.com>
2025-02-01 15:03:43 +0100

    fix: headroom not hiding on scroll after tailwind update

--- src/lib/components/Headroom.svelte
+++ src/lib/components/Headroom.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { fly } from 'svelte/transition';
 	import type { Snippet } from 'svelte';
+	import { cn } from '$lib/utils';
 
 	interface Props {
 		offset?: number;
@@ -45,9 +46,11 @@
 
 <div class="h-[300px]">
 	<header
-		class={`fixed z-50 flex w-full flex-col to-transparent transition-all duration-700 after:absolute after:top-0 after:-z-10 after:contents after:w-full after:bg-linear-to-b after:from-white after:to-transparent after:backdrop-blur-[2px] after:transition-all after:duration-1000 dark:after:from-black ${classesHeaderDocked}`}
+		class={cn(
+			'fixed -top-20 z-50 flex w-full flex-col to-transparent transition-all duration-700 after:absolute after:top-0 after:-z-10 after:contents after:w-full after:bg-linear-to-b after:from-white after:to-transparent after:backdrop-blur-[2px] after:transition-all after:duration-1000 dark:after:from-black',
+			classesHeaderDocked
+		)}
 		class:top-0={headerVisible}
-		class:-top-20={!headerVisible}
 	>
 		{@render children?.()}
 		{#if !headerDocked && headerVisible}
